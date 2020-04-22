29585f4c3dbc6f1cfaa5bf8f7877517f4764c0e0
luis carlos mora villalba <ingmoravillalba@gmail.com>
2020-04-22 14:43:51 -0400

    Updates

---
+++ precache-manifest.8b46d30f74981e7c03ec175676513d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf5dab8cc651b6bce93722b73a48079",
+    "url": "/shakafar/todo-app.git/index.html"
+  },
+  {
+    "revision": "b4dc9b5eeaf1f2c60fa3",
+    "url": "/shakafar/todo-app.git/static/css/main.399b6524.chunk.css"
+  },
+  {
+    "revision": "81d76b98cef813061a95",
+    "url": "/shakafar/todo-app.git/static/js/2.ab40c02d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shakafar/todo-app.git/static/js/2.ab40c02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dc9b5eeaf1f2c60fa3",
+    "url": "/shakafar/todo-app.git/static/js/main.16db2e40.chunk.js"
+  },
+  {
+    "revision": "28b256d362c6d762453a",
+    "url": "/shakafar/todo-app.git/static/js/runtime-main.e9c32ecb.js"
+  }
+]);
